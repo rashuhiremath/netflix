40d03de8df36068c8df385be52128966e6a1699b
rashuhiremath <rashmi.hiremath94@gmail.com>
2021-10-29 17:19:30 +0200

    mplk;

---
+++ src/components/MovieDetails.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const MovieDetails = ({ match }) => {
+  const [movieItem, setMovieItem] = useState(null);
+
+  useEffect(() => {
+    let urlId = match.params.id;
+    fetchMovie(urlId);
+  }, []);
+
+  const fetchMovie = async (urlId) => {
+    try {
+      const response = await fetch(process.env.REACT_APP_GET_URL+urlId
+       // `http://localhost:3001/media=${urlId}`
+      );
+      const imageData = await response.json();
+      if (response.ok) {
+        console.log("my single movie details", imageData);
+        setMovieItem(imageData);
+      } else {
+        console.log(`something went wrong`);
+      }
+    } catch (e) {
+      console.error(`ooops an error occured while fetching`, e);
+    }
+  };
+
+  return (
+    <div className="text-white">
+      {
+      movieItem && <div>{<img src={movieItem.Poster} />}</div>
+      }
+    </div>
+  );
+};
+export default MovieDetails;
